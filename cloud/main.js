a2fb8de8980740e6dd9d88c4dd0af7afcdd5bcd6
Waseem Ahmed <codewaseem@gmail.com>
2019-04-24 21:54:20 +0530

    deleteUserByEmail

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,13 @@
-Parse.Cloud.define('hello', function(req, res) {
-  return 'Hi';
+Parse.Cloud.define("deleteUserByEmail", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var email = request.params.email;
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("email", email);
+  query.first().then(function(user) {
+      return user.destroy();
+  }).then(function() {
+      response.success(user);
+  }, function(error) {
+      response.error(error);
+  });
 });
